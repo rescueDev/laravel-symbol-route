ad95b9e54b103c310559f7361b2a769e07ba1992
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-26 18:53:22 +0100

    conditional route link

--- laravel-payments/resources/views/pages/pagamenti.blade.php
+++ laravel-payments/resources/views/pages/pagamenti.blade.php
@@ -15,6 +15,25 @@
             {{ $pagamento['created_at'] }}
         </h4>
 
+
+
     @endforeach
+
+    @php
+    $name = \Request::url();
+    //var_dump($name);
+
+    if ($name == "http://localhost:8000/pagamentipending") {
+    $rotta = 'pagamenti';
+    }
+    else if ($name == "http://localhost:8000/pagamenti") {
+    $rotta = 'pagamentipending';
+    }
+
+    //var_dump($rotta);
+    @endphp
+
+    <a href="{{ route($rotta) }}">{{ $rotta }}</a>
+
 
 @endsection
