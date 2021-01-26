9dd10669c259ab5da81025e1601b56b4f6f57403
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-26 15:55:04 +0100

    first tak completed

--- laravel-payments/resources/views/pages/pagamenti.blade.php
+++ laravel-payments/resources/views/pages/pagamenti.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.main-layout')
 @section('content')
 
+    @foreach ($pagamenti as $pagamento)
+        @php
+        if($loop->odd) {
+        $class = "green";
+        }
+        else {
+        $class = "red";
+        }
+        @endphp
+
+        <h4 class="{{ $class }}"> {{ $pagamento['id'] }} - {{ $pagamento['status'] }} - {{ $pagamento['price'] }} $ -
+            {{ $pagamento['created_at'] }}
+        </h4>
+
+    @endforeach
+
 @endsection
